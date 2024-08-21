4b140070619b36b9f4f6716851a1131f195b209d
Mohammed Aqeeb <mohammedaqeeb19@gmail.com>
2024-08-21 23:58:12 +0530

    Stop tracking app_keys.dart

--- lib/core/keys/app_keys.dart
+++
@@ -1,7 +0,0 @@
-class AppKeys {
-  static const supabaseUrl = 'https://iblftqfxbtzkrgrhtgiq.supabase.co';
-
-  
-  static const anonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlibGZ0cWZ4YnR6a3Jncmh0Z2lxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE3MDU2MzIsImV4cCI6MjAyNzI4MTYzMn0.MfE1uPN7WE7Vzlx0BZYMmBi-tMNonaTsZ1S0cjYWCbk';
-}
